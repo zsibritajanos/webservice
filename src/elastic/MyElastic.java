2fa1e9d8dd15fe285ccdf95dd0b5fb96a70a864b
zsibritajanos <zsibrita.janos@gmail.com>
2016-10-27 17:36:46 +0200

    empty tomcat

---
+++ src/elastic/MyElastic.java
@@ -0,0 +1,16 @@
+package elastic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by zsjanos on 2016.10.25..
+ */
+public class MyElastic {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
